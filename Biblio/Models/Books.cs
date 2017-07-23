970711119dc8955161169d08283b177c571a4d4b
TL0CK <appartement130@gmail.com>
2017-07-23 17:07:42 +0200

    ajout de model

---
+++ Biblio/Models/Books.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Biblio.Models
+{
+    public class Books
+    {
+        public int ID { get; set; }
+        public string Title { get; set; }
+        public decimal Stock { get; set; }
+    }
+}
